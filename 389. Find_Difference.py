5f4d16a24844e96688be2f64d5934b9f21b13f93
Adarsh Rai <101463523+Adarshrai1@users.noreply.github.com>
2023-09-26 12:24:52 +0530

    Create 389. Find_Difference.py

---
+++ 389. Find_Difference.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        char_count = {}
+        for char in s:
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+        for char in t:
+            if char in char_count and char_count[char] > 0:
+                char_count[char] -= 1
+            else:
+                return char
+
+        return ''  
+        
