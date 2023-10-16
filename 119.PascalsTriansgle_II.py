188098754f2080f7340d67b593eaa739f198ea9a
Adarsh Rai <101463523+Adarshrai1@users.noreply.github.com>
2023-10-17 00:39:06 +0530

    Create 119.PascalsTriansgle_II.py

---
+++ 119.PascalsTriansgle_II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex < 0:
+            return []
+
+        row = [1]
+        for i in range(1, rowIndex + 1):
+            next_row = [1]
+            for j in range(1, i):
+                next_row.append(row[j - 1] + row[j])
+            next_row.append(1)
+            row = next_row
+
+        return row  #Adarsh_Rai
